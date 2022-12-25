2b503dcc88ae8139db2e8db125df3eacd6ba5f09
JamesOmare <jamesomare922@gmail.com>
2022-12-25 11:48:36 +0300

    added product and category views

--- multivendor/core/views.py
+++ multivendor/core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from store.models import Product
+
 # Create your views here.
 def frontpage(request):
-    return render(request, 'core/')
+    products = Product.objects.all()[0:6]
+    context = {
+        'products': products
+    }
+    return render(request, 'core/frontpage.html', context)
+
+def about(request):
+    return render(request, 'core/about.html')
