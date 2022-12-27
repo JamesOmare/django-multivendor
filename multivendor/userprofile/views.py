f39da64e8b81ec4c8f22b4b2c438850e886f3564
JamesOmare <jamesomare922@gmail.com>
2022-12-27 18:50:56 +0300

    added authentication and search feature

--- multivendor/userprofile/views.py
+++ multivendor/userprofile/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from .models import Userprofile
+
 
 # Create your views here.
+
+def vendor_details(request, pk):
+    user = User.objects.get(pk = pk)
+    context = {
+        'user': user,
+    }
+
+    return render(request, 'userprofile/vendor_details.html', context)
+
+
+def myaccount(request):
+    return render(request, 'userprofile/myaccount.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            userprofile = Userprofile.objects.create(user = user)
+
+            return redirect('frontpage')
+
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'userprofile/signup.html', context)
+
+        
